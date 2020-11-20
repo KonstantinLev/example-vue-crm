a3193cea81da51ac7082973cbd7241a209c36d4f
Konstantin <levachin@luckandroll.com>
2020-11-21 01:21:58 +0300

    added firebase login

--- src/main.js
+++ src/main.js
@@ -8,14 +8,36 @@ import msgPlugin from '@/utils/msg.plugin'
 import './registerServiceWorker'
 import 'materialize-css/dist/js/materialize.min'
 
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
+
 Vue.config.productionTip = false
 
 Vue.use(Vuelidate)
 Vue.use(msgPlugin)
 Vue.filter('date', dateFilter)
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+
+// Initialize Firebase
+firebase.initializeApp({
+  apiKey: "AIzaSyAP0yZvhIfJhvn--gNmprYYuyQv7qjAVhc",
+  authDomain: "example-vue-crm.firebaseapp.com",
+  databaseURL: "https://example-vue-crm.firebaseio.com",
+  projectId: "example-vue-crm",
+  storageBucket: "example-vue-crm.appspot.com",
+  messagingSenderId: "287173384731",
+  appId: "1:287173384731:web:60981f41e046dfbd4bb6be"
+});
+
+let app
+
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount('#app')
+  }
+})
