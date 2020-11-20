8e2af8fc5ceb0227f660d3dd11e32eecc5628a65
Konstantin <levachin@luckandroll.com>
2020-11-21 02:12:57 +0300

    get username from bd

--- src/components/app/Navbar.vue
+++ src/components/app/Navbar.vue
@@ -16,7 +16,7 @@
             data-target="dropdown"
             ref="dropdown"
           >
-            User
+            {{name}}
             <i class="material-icons right">arrow_drop_down</i>
           </a>
 
@@ -53,6 +53,11 @@
       this.interval = setInterval(() => {
         this.date = new Date()
       }, 1000)
+    },
+    computed: {
+      name() {
+        return this.$store.getters.info.name
+      }
     },
     methods: {
       async logout() {
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -30,6 +30,11 @@
     }),
     components: {
       Navbar, Sidebar
+    },
+    async mounted() {
+      if (!Object.keys(this.$store.getters.info).length) {
+        await this.$store.dispatch('fetchInfo')
+      }
     }
   }
 </script>
