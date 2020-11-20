f3a0df7f1a7d0473bd3dae98f5b8e4451a859d2b
Konstantin <levachin@luckandroll.com>
2020-11-20 21:53:38 +0300

    added views, added some components, configure main layout

---
+++ src/views/Planning.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div class="page-title">
+      <h3>Планирование</h3>
+      <h4>12 212</h4>
+    </div>
+
+    <section>
+      <div>
+        <p>
+          <strong>Ламборджини:</strong>
+          1008 из 14 000 000
+        </p>
+        <div class="progress" >
+          <div
+                  class="determinate green"
+                  style="width:10%"
+          ></div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
