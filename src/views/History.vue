120efda3b28c968ba5ceedba567f8788ef414233
Konstantin <levachin@luckandroll.com>
2020-11-22 16:18:55 +0300

    added pagination mixin

--- src/views/History.vue
+++ src/views/History.vue
@@ -13,36 +13,48 @@
     <p class="center" v-else-if="!records.length">Записей необнаружено <router-link to="/record">Добавьте их!</router-link></p>
 
     <section v-else>
-      <Table :records="records" />
+      <Table :records="items" />
+
+      <Paginate
+        v-model="page"
+        :page-count="pageCount"
+        :click-handler="pageChangeHandler"
+        :prev-text="'Сюда'"
+        :next-text="'Туда'"
+        :container-class="'pagination'"
+        :page-class="'waves-effect'"
+      />
     </section>
   </div>
 </template>
 
 <script>
+  import paginationMixin from '../mixins/pagination.mixin'
   import Table from "../components/history/Table";
   export default {
     name: 'history',
+    mixins: [paginationMixin],
     components: {
       Table
     },
     data: () => ({
       loading: true,
       records: [],
-      categories: []
+      //categories: []
     }),
     async mounted() {
-      //this.records = await this.$store.dispatch('fetchRecords')
-      this.categories = await this.$store.dispatch('fetchCategories')
-      const records = await this.$store.dispatch('fetchRecords')
+      this.records = await this.$store.dispatch('fetchRecords')
+      const categories = await this.$store.dispatch('fetchCategories')
+      //const records = await this.$store.dispatch('fetchRecords')
 
-      this.records = records.map(record => {
+      this.setupPagination(this.records.map(record => {
         return {
           ...record,
-          categoryName: this.categories.find(c => c.id === record.categoryId).title,
+          categoryName: categories.find(c => c.id === record.categoryId).title,
           typeClass: record.type === 'income' ? 'green' : 'red',
           typeText: record.type === 'income' ? 'Доход' : 'Расход',
         }
-      })
+      }))
 
 
       this.loading = false
